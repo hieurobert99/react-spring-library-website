5a34987c1569ea745fcf4cd17ca518c3dcae2b3e
hieurobert <hieurobert@gmail.com>
2024-03-19 01:18:02 +0100

    oktaAuth seems to break the program, but the rest works

--- src/layouts/NavbarAndFooter/Navbar.tsx
+++ src/layouts/NavbarAndFooter/Navbar.tsx
@@ -1,32 +1,53 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { useOktaAuth } from '@okta/okta-react';
+import { SpinnerLoading } from "../Utils/SpinnerLoading";
 
 export const Navbar = () => {
-    return(
-        <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
+
+  const { oktaAuth, authState } = useOktaAuth();
+
+  if (!authState) {
+    return <SpinnerLoading />
+  }
+
+  const handleLogout = async () => oktaAuth.signOut();
+
+  console.log(authState);
+
+
+  return (
+    <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
       <div className='container-fluid'>
         <span className='navbar-brand'>Luv 2 Read</span>
-        <button className='navbar-toggler' type="button"
-          data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-          aria-controls='navbarNavDropdown' aria-expanded ='false'
-          aria-label='Toggle Navigation'>
-            <span className='navbar-toggle-icon'></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className='navbar-nav'>
-              <li className='nav-item'>
-                <NavLink className='nav-link' to='/home'>Home</NavLink>
+        <button className='navbar-toggler' type='button'
+          data-bs-toggle='collapse' data-bs-target='#navbarNavDropdown'
+          aria-controls='navbarNavDropdown' aria-expanded='false'
+          aria-label='Toggle Navigation'
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
+        <div className='collapse navbar-collapse' id='navbarNavDropdown'>
+          <ul className='navbar-nav'>
+            <li className='nav-item'>
+              <NavLink className='nav-link' to='/home'>Home</NavLink>
+            </li>
+            <li className='nav-item'>
+              <NavLink className='nav-link' to='/search'>Search Books</NavLink>
+            </li>
+          </ul>
+          <ul className='navbar-nav ms-auto'>
+            {!authState.isAuthenticated ?
+              <li className='nav-item m-1'>
+                <Link type='button' className='btn btn-outline-light' to='/login'>Sign in</Link>
               </li>
-              <li className='nav-item'>
-                <NavLink className='nav-link' to='/search'>Search Books</NavLink>
+              :
+              <li>
+                <button className='btn btn-outline-light' onClick={handleLogout}>Logout</button>
               </li>
-            </ul>
-            <ul className='navbar-nav ms-auto'>
-                <li className='nav-item m-1'>
-                  <a type="button" className='btn btn-outline-light' href='#'>Sign In</a>
-                </li>
-            </ul>
-          </div>
+            }
+          </ul>
+        </div>
       </div>
     </nav>
-    );
+  );
 }
